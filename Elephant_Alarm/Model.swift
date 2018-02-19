9619930792aa001f31fedb2bf353132b158458be
stevewreese <stephenw.reese@gmail.com>
2018-02-18 22:45:46 -0700

    fixed

--- Elephant_Alarm/Model.swift
+++ Elephant_Alarm/Model.swift
@@ -20,22 +20,15 @@ struct imported
     var label: String
     var time: String
     var day: String
-<<<<<<< HEAD
     var zone: String
     var repeating: Int
     init(label: String, time: String, day: String, zone: String, repeating: Int)
-=======
-    init(label: String, time: String, day: String)
->>>>>>> origin/master
     {
         self.label = label
         self.time = time
         self.day = day
-<<<<<<< HEAD
         self.zone = zone
         self.repeating = repeating
-=======
->>>>>>> origin/master
     }
 }
 
@@ -100,11 +93,7 @@ class Model
     {
         var toExport: String = ""
         for view in views {
-<<<<<<< HEAD
             toExport = "\(toExport)\(view.EventName)_\(view.seconds)_\(view.Week_Day)_\(view.TimeZone)_\(view.repeatVal)#"
-=======
-            toExport = "\(toExport)\(view.EventName)_\(view.seconds)_\(view.Week_Day)#"
->>>>>>> origin/master
         }
         print(toExport)
         let theNSExport: NSString = toExport as NSString
